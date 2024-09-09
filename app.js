41d8b34ac64421050af90843ed8e4fc7b124addd
Gustavo Hammes <daffa1632@gmail.com>
2024-09-09 18:06:51 -0300

    Modificação do código

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function pesquisar() {
     for (let dado of dados) {
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
+        
         // se titulo includes campoPesquisa
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
             // cria um novo elemento
